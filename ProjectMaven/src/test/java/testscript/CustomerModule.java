05f246202890030737ce444c46d5b0d6c5f21563
Dell <Dell@KOUSHIK>
2024-12-27 10:56:46 +0530

    commit

---
+++ ProjectMaven/src/test/java/testscript/CustomerModule.java
@@ -0,0 +1,26 @@
+package testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+	public class CustomerModule {
+	@Test
+	
+	public void createCampaign() {
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com");
+		
+		System.out.println("The title of web page is  " +driver.getTitle());
+		
+		Reporter.log("createCampaign",true);
+		
+		driver.quit();
+		
+	}
+	
+}
